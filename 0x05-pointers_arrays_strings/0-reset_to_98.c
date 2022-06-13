0b4bfc9c2fb357cffefed609b095fa361531aff1
Randy0607 <randall.julies89@gmail.com>
2022-06-13 12:11:22 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
-#include "holberton.h"
-/**
- * reset_to_98 - reset argument to 98
- * @n: pointer
- * Return: void
-*/
+#include "main.h"
 
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: integer
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
-
-*n = 98;
-
+	*n = 98;
 }
