a41a3ca3ccad5632bacf44418e3c3cf00d9a6def
Randy0607 <randall.julies89@gmail.com>
2022-06-16 14:33:13 -0700

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * reverse_array - reverse a int array
  * @a: pointer
@@ -15,5 +15,4 @@ for (half = n / 2; half > 0; half--, i++)
 	a[n - i - 1] += a[i];
 	a[i] = a[n - i - 1] - a[i];
 	a[n - i - 1] = a[n - i - 1] - a[i];
-}
 }
