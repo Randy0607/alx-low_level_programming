db1ece1905615e82782162d681cd1178b046a36c
Randy0607 <randall.julies89@gmail.com>
2022-06-24 12:51:15 -0700

    commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
 #ifndef main.h
-#define main.h
+#define "main.h"
 
 /**
  *
@@ -29,3 +29,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
