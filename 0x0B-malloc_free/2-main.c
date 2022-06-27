ecc688fae8daecea0973a853ab280123fd96ded7
Randy0607 <randall.julies89@gmail.com>
2022-06-27 08:53:56 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
@@ -11,11 +11,11 @@ int main(void)
 {
     char *s;
 
-    s = str_concat("NULL", " NULL");
+    s = str_concat("Betty ", " Holberton");
     if (s == NULL)
     {
-        printf("failed\n");
-        return (1);
+	    printf("failed\n");
+	    return (1);
     }
     printf("%s\n", s);
     free(s);
