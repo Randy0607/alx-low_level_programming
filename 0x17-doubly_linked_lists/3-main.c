dd0d81fedba2b7487d614bef967e2ba76e69bd70
Randy0607 <randall.julies89@gmail.com>
2022-09-01 20:03:36 +0100

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/3-main.c
+++ 0x17-doubly_linked_lists/3-main.c
@@ -11,7 +11,6 @@
 int main(void)
 {
     dlistint_t *head;
-    int sum;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -22,9 +21,6 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    sum = sum_dlistint(head);
-    printf("sum = %d\n", sum);
-    free_dlistint(head);
-    head = NULL;
+    print_dlistint(head);
     return (EXIT_SUCCESS);
 }
